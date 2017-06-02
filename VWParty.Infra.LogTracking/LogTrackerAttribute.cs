195a817cc57f7a63b6418016276377f8b6787437
Andrew Wu <andrewwu@66.net>
2017-06-02 18:00:07 +0800

    add try catch to prevent 500 error

--- VWParty.Infra.LogTracking/LogTrackerAttribute.cs
+++ VWParty.Infra.LogTracking/LogTrackerAttribute.cs
@@ -12,71 +12,54 @@ namespace VWParty.Infra.LogTracking
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class LogTrackerAttribute : ActionFilterAttribute
     {
-        public bool IsCreateRequestId { get { return true; } }
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
 
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
-            Logger _log = LogManager.GetLogger("LogTracker", actionContext.ControllerContext.Controller.GetType());
-
-            //if (actionContext.Request.Headers.Contains("X-REQUEST-ID") == false)
-            //{
-            //    _log.Info("create req id");
-            //    actionContext.Request.Headers.Add("X-REQUEST-ID", Guid.NewGuid().ToString());
-            //    actionContext.Request.Headers.Add("X-REQUEST-UTCTIME", DateTime.UtcNow.ToString("u"));
-            //}
-            if (LogTrackerContext.Current == null)
+            try
             {
-                _log.Info("create req id");
-                LogTrackerContext.Init(LogTrackerContextStorageTypeEnum.ASPNET_HTTPCONTEXT);
+                Logger _log = LogManager.GetLogger("LogTracker", actionContext.ControllerContext.Controller.GetType());
+                if (LogTrackerContext.Current == null)
+                {
+                    _log.Info("create req id");
+                    LogTrackerContext.Init(LogTrackerContextStorageTypeEnum.ASPNET_HTTPCONTEXT);
+                }
+                _log.Info(string.Format(
+                    "before call (req id: {0}, utctime: {1}, execute-ms: {2})",
+                    LogTrackerContext.Current.RequestId,
+                    LogTrackerContext.Current.RequestStartTimeUTC,
+                    LogTrackerContext.Current.RequestExecutingTime.TotalMilliseconds));
             }
-
-            //_log.Info(
-            //    "before call (req id: {0}, utctime: {1}, execute-ms: {2})",
-            //    LogTrackerContext.Current.RequestId,
-            //    LogTrackerContext.Current.RequestStartTimeUTC,
-            //    LogTrackerContext.Current.RequestExecutingTime.TotalMilliseconds);
-
-            _log.Info(string.Format(
-                "before call (req id: {0}, utctime: {1}, execute-ms: {2})",
-                LogTrackerContext.Current.RequestId,
-                LogTrackerContext.Current.RequestStartTimeUTC,
-                LogTrackerContext.Current.RequestExecutingTime.TotalMilliseconds));
-
-
-            //_log.Info("-----------------------------------------------------------------------");
-            //foreach (var header in actionContext.Request.Headers)
-            //{
-            //    _log.Info("- header [{0}]: {1}", header.Key, header.Value.FirstOrDefault());
-            //}
-            //_log.Info("-----------------------------------------------------------------------");
-
+            catch(Exception ex)
+            {
+                _logger.Warn(ex);
+            }
             base.OnActionExecuting(actionContext);
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
-            Logger _log = LogManager.GetLogger("LogTracker", actionExecutedContext.ActionContext.ControllerContext.Controller.GetType());
+            try { 
+                Logger _log = LogManager.GetLogger("LogTracker", actionExecutedContext.ActionContext.ControllerContext.Controller.GetType());
 
-            //_log.Info(
-            //    "after call (req id: {0}, utctime: {1}, execute-ms: {2})",
-            //    LogTrackerContext.Current.RequestId,
-            //    LogTrackerContext.Current.RequestStartTimeUTC,
-            //    LogTrackerContext.Current.RequestExecutingTime.TotalMilliseconds);
+                _log.Info(string.Format(
+                    "after call (req id: {0}, utctime: {1}, execute-ms: {2})",
+                    LogTrackerContext.Current.RequestId,
+                    LogTrackerContext.Current.RequestStartTimeUTC,
+                    LogTrackerContext.Current.RequestExecutingTime.TotalMilliseconds));
 
-            _log.Info(string.Format(
-                "after call (req id: {0}, utctime: {1}, execute-ms: {2})",
-                LogTrackerContext.Current.RequestId,
-                LogTrackerContext.Current.RequestStartTimeUTC,
-                LogTrackerContext.Current.RequestExecutingTime.TotalMilliseconds));
+                actionExecutedContext.Response.Headers.Add(
+                    LogTrackerContext._KEY_REQUEST_ID,
+                    LogTrackerContext.Current.RequestId);
 
-
-            actionExecutedContext.Response.Headers.Add(
-                LogTrackerContext._KEY_REQUEST_ID,
-                LogTrackerContext.Current.RequestId);
-
-            actionExecutedContext.Response.Headers.Add(
-                LogTrackerContext._KEY_REQUEST_START_UTCTIME,
-                LogTrackerContext.Current.RequestStartTimeUTC.ToString("u"));
+                actionExecutedContext.Response.Headers.Add(
+                    LogTrackerContext._KEY_REQUEST_START_UTCTIME,
+                    LogTrackerContext.Current.RequestStartTimeUTC.ToString("u"));
+            }
+            catch (Exception ex)
+            {
+                _logger.Warn(ex);
+            }
 
             base.OnActionExecuted(actionExecutedContext);
         }
